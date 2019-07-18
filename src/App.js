1dcfa61843e760306623f80df14871f98edb66ac
Tyler McKee <v808120@Tylers-MacBook-Pro.local>
2019-07-17 21:07:51 -0500

    dev

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
       <div className="App">
         <header>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Dev</h1>
         </header>
         <div className="App-intro">
           <button className="App-button" onClick={this.handleAnalyticsClick}>Generate Analytics Event</button>
